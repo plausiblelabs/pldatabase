099ac25efdbd20b01b9f0b747c831d343992a5fc
Landon Fuller <landonf@plausible.coop>
2008-05-03 01:10:30 +0000

    Skeleton the database protocol and the sqlite implementation
    git-svn-id: https://pldatabase.googlecode.com/svn/trunk@4 3603cd71-484c-0410-acc4-09e5374cfb26

---
+++ Classes/PLSqliteDatabase.m
@@ -0,0 +1,13 @@
+//
+//  PLSqliteDatabase.m
+//  PlausibleDatabase
+//
+//  Created by Landon Fuller on 5/2/08.
+//  Copyright 2008 Plausible Labs. All rights reserved.
+//
+
+#import "PlausibleDatabase.h"
+
+@implementation PLSqliteDatabase
+
+@end
---
+++ Classes/PLSqliteDatabaseTests.m
@@ -0,0 +1,21 @@
+//
+//  PLSqliteDatabaseTests.m
+//  PlausibleDatabase
+//
+//  Created by Landon Fuller on 5/2/08.
+//  Copyright 2008 Plausible Labs. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface PLSqliteDatabaseTests : SenTestCase
+@end
+
+
+@implementation PLSqliteDatabaseTests
+
+- (void) testSomething {
+    // Whee
+}
+
+@end
